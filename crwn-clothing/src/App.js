fbf488651ca248065426954a1981f123f4c8dc6c
Jlee0814 <w207646952@gmail.com>
2020-04-26 23:49:46 -0500

    update menu with withRouter HOC

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import {Route} from'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+const Hatspage=()=>(
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Route exact path='/' component={HomePage}/>
+      <Route exact path='/shop/hats' component={Hatspage}/>
+      <Route exact path='/shop/jackets' component={Hatspage}/>
+      <Route exact path='/shop/sneakers' component={Hatspage}/>
+      <Route exact path='/shop/womens' component={Hatspage}/>
+      <Route exact path='/shop/mens' component={Hatspage}/>
     </div>
   );
 }
